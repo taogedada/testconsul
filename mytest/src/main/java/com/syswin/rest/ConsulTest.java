8150ed97a3f4bb483e03aa7c57198a3336baf2bf
taogedada <49275730+taogedada@users.noreply.github.com>
2019-06-01 23:30:08 +0800

    Update ConsulTest.java

--- mytest/src/main/java/com/syswin/rest/ConsulTest.java
+++ mytest/src/main/java/com/syswin/rest/ConsulTest.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConsulTest {
 int a = 1;
+    int z = 1;
 //    @Autowired
 //    private LoadBalancerClient loadBalancer;
 //    @Autowired
