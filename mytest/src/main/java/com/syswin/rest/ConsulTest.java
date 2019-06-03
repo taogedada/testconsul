392f1aa37f456b0ea42a8dde2e25bb70abd69258
taogedada <837089174@qq.com>
2019-06-03 17:36:06 +0800

    Revert "测试7"

--- mytest/src/main/java/com/syswin/rest/ConsulTest.java
+++ mytest/src/main/java/com/syswin/rest/ConsulTest.java
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ConsulTest {
-
+int a = 1;
+int j = 1;
+int d = 2;
 //    @Autowired
 //    private LoadBalancerClient loadBalancer;
 //    @Autowired
