09a865521b7466ebb679e88da59c3c2ebd4de43d
taogedada <837089174@qq.com>
2019-06-03 17:39:37 +0800

    Revert "测试9"
    This reverts commit d7ed08b0

--- mytest/src/main/java/com/syswin/rest/ConsulTest.java
+++ mytest/src/main/java/com/syswin/rest/ConsulTest.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ConsulTest {
-int a = 1;
+
 //    @Autowired
 //    private LoadBalancerClient loadBalancer;
 //    @Autowired
